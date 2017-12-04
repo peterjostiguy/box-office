f4f2ac01a182dbcc50e2991a13fef477a414b25e
peterjostiguy <peterjostiguy@gmail.com>
2017-12-04 09:57:58 -0700

    fixed typo

--- routes/users.js
+++ routes/users.js
@@ -27,7 +27,7 @@ router.post('/signup', function(req, res, next) {
   res.render('index', {user: req.body.username})
 })
 
-router.post('signin', function(req, res, next){
+router.post('/signin', function(req, res, next){
   return knex('my_user').insert({agentName: agentName, password: password}, "password")
   ref.once('/users/').once('value', function(snapshot){
     console.log(snapshot)
