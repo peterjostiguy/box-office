daabdc1afa3df3af1be924911a5b65d7705d7c54
peterjostiguy <peterjostiguy@gmail.com>
2017-12-04 09:42:15 -0700

    fixed post

--- routes/users.js
+++ routes/users.js
@@ -19,6 +19,7 @@ ref.once("value", function(snapshot) {
 var usersRef = ref.child("users")
 
 router.post('/signup', function(req, res, next) {
+  // This doesn't protect against duplicates
   bcrypt.hash(req.body.password, 10, function(err, hash){
     req.body.password = hash
     usersRef.push().set(req.body)
