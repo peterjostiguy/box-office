7ab0b270672bbe0a7da95bdab6f191a92cb7c65f
peterjostiguy <peterjostiguy@gmail.com>
2017-12-04 13:44:21 -0700

    working on routes

--- routes/users.js
+++ routes/users.js
@@ -2,7 +2,6 @@ var express = require('express')
 var router = express.Router()
 var admin = require('firebase-admin')
 var bcrypt = require('bcrypt')
-
 var serviceAccount = require("../firebase-boxoffice.json")
 
 admin.initializeApp({
@@ -12,10 +11,6 @@ admin.initializeApp({
 
 var db = admin.database()
 var ref = db.ref()
-ref.once("value", function(snapshot) {
-  //This returns all data from FB DB
-  // console.log(snapshot.val())
-})
 var usersRef = ref.child("users")
 
 router.post('/signup', function(req, res, next) {
@@ -35,35 +30,28 @@ router.post('/signin', function(req, res, next){
     var userExist = false
     var authMessage = "You didn't sign up ya dumb bitch"
     for (var i = 0; i < dataValues.length; i++) {
-      console.log("compare ", dataValues[i].username, req.body.username );
       if (dataValues[i].username === req.body.username) {
         userExist = true
-        console.log("match");
         var user = dataValues[i]
         bcrypt.compare(req.body.password, user.password, function(err, isMatch){
-          console.log("BCRYPTIN", isMatch)
-
           if (isMatch) {
-            authMessage = "welcome back" + user
-            console.log("correct", authMessage)
+            authMessage = "welcome back" + user.username
+            res.redirect('https://box-office-fantasy.firebaseapp.com/user.html')
             res.send(authMessage)
           }
           else {
-            authMessage = "You done fucked up" + user
-            console.log("wrong password", authMessage)
+            authMessage = "You done fucked up" + user.username
+            res.redirect('https://box-office-fantasy.firebaseapp.com/user.html')
             res.send(authMessage)
           }
         })
-        // var user = dataValues[i]
-        // res.send(user)
         break
       }
     }
     if (!userExist) {
-      console.log("well, this happened")
+      res.redirect('https://box-office-fantasy.firebaseapp.com/user.html')
       res.send(authMessage)
     }
-
   })
 })
 
